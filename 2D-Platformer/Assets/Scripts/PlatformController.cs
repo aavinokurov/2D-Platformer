e7aa5e2dec987e11145c16035605ab0115e62a16
AlexandrVinokurov <aavinokurov@outlook.com>
2021-04-13 18:12:33 +0300

    Add decoration, rigid body and move platform

---
+++ 2D-Platformer/Assets/Scripts/PlatformController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    [SerializeField] private SliderJoint2D platform;
+    private bool isForward = false;
+    private JointMotor2D platformMotor;
+
+    private void FixedUpdate()
+    {
+
+        if (platform.jointTranslation <= 0.1f)
+        {
+            if (!isForward)
+            {
+                isForward = true;
+            }
+        }
+
+        if (platform.jointTranslation >= 1.3f)
+        {
+            if (isForward)
+            {
+                isForward = false;
+            }
+        }
+
+        platformMotor = platform.motor;
+
+        if (isForward)
+        {
+            platformMotor.motorSpeed = 0.5f;
+        }
+        else
+        {
+            platformMotor.motorSpeed = -0.5f;
+        }
+
+        platform.motor = platformMotor;
+    }
+}
---
+++ 2D-Platformer/Assets/Scripts/SkullExplosion.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullExplosion : MonoBehaviour
+{
+    [SerializeField] private GameObject demon;
+    [SerializeField] private Transform parent;
+    [SerializeField] private bool isDead;
+    [SerializeField] private GameObject skull;
+    [SerializeField] private GameObject skullFire;
+
+    private void FixedUpdate()
+    {
+        if (isDead)
+        {
+            skull.SetActive(true);
+            skullFire.SetActive(true);
+            demon.SetActive(false);
+            Instantiate(skull, parent);
+            Instantiate(skullFire, parent);
+            StartCoroutine(explosionTimer());
+        }
+    }
+
+    private IEnumerator explosionTimer()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        isDead = false;
+    }
+}

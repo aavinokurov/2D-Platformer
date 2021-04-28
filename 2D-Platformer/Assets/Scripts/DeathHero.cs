2500f4b13294b907720ccc154d59b30165736063
AlexandrVinokurov <aavinokurov@outlook.com>
2021-04-28 19:22:56 +0300

    Add death hero and attack for demon

---
+++ 2D-Platformer/Assets/Scripts/DeathHero.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class DeathHero : MonoBehaviour
+{
+    [SerializeField] private GameObject deathPanel;
+    [SerializeField] private Health healthHero;
+    private void OnTriggerEnter2D(Collider2D deathFloor)
+    {
+        if (deathFloor.CompareTag("Death"))
+        {
+            Death();
+        }
+    }
+
+    private void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    private void Update()
+    {
+        if (!healthHero.isAlive)
+        {
+            Death();
+        }
+    }
+
+    private void Death()
+    {
+        deathPanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
---
+++ 2D-Platformer/Assets/Scripts/DemonCollision.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemonCollision : MonoBehaviour
+{
+    [SerializeField] private float damage;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.layer == 11)
+        {
+            other.gameObject.GetComponent<Health>().TakeDamage(damage);
+        }
+    }
+}

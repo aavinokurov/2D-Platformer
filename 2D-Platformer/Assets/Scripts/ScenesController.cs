328b6360868808c968d9a80122caafa10bf5eab4
AlexandrVinokurov <aavinokurov@outlook.com>
2021-05-09 22:09:42 +0300

    Start level 2

--- 2D-Platformer/Assets/Scripts/ScenesController.cs
+++ 2D-Platformer/Assets/Scripts/ScenesController.cs
@@ -14,4 +14,24 @@ public class ScenesController : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+
+    public void LoadLevel(int numLevel)
+    {
+        SceneManager.LoadScene(numLevel);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void ContinueGame()
+    {
+        Time.timeScale = 1;
+    }
 }

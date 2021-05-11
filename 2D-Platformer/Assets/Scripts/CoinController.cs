ccdf9de690c9a25e3151fff65fddf3da271cba1d
AlexandrVinokurov <aavinokurov@outlook.com>
2021-05-11 23:00:06 +0300

    Add red coin

---
+++ 2D-Platformer/Assets/Scripts/CoinController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinController : MonoBehaviour
+{
+    [SerializeField] private Text countCoinText;
+
+    private static int countCoin;
+
+    private void Start()
+    {
+        countCoin = 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == 11)
+        {
+            countCoin++;
+            countCoinText.text = countCoin.ToString();
+            Destroy(gameObject);
+        }
+    }
+}

fc2c058c666417560fb5c6282efd7a79cbc25087
AlexandrVinokurov <aavinokurov@outlook.com>
2021-05-12 13:13:04 +0300

    Add create item

---
+++ 2D-Platformer/Assets/Scripts/CreateItems.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateItems : MonoBehaviour
+{
+    [SerializeField] private GameObject item;
+    [SerializeField] private Transform createPosition;
+    [SerializeField] private Health healthEnemy;
+
+    private void Update()
+    {
+        if (!healthEnemy.isAlive)
+        {
+            Instantiate(item,createPosition.position,Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
